82452a6de5ffdcbd22f07f6f27869f04f027dd61
drocpdp <davidreynon@gmail.com>
2023-05-20 17:10:43 +0200

    Search section initial layout completed

--- restaurants/lib/widgets/search_section.dart
+++ restaurants/lib/widgets/search_section.dart
@@ -9,44 +9,69 @@ class SearchSection extends StatefulWidget {
 
 class SearchSectionState extends State<SearchSection> {
   Widget searchSection = Container(
-      padding: const EdgeInsets.all(25),
-      alignment: Alignment.topCenter,
-      height: 125,
-      color: Colors.green,
-      child: Row(children: [
+    padding: const EdgeInsets.all(25),
+    decoration: BoxDecoration(
+      color: Color.fromARGB(95, 255, 253, 253),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.2),
+          spreadRadius: 5,
+          blurRadius: 10,
+          offset: const Offset(0, 3),
+        ),
+      ],
+      border: Border.all(
+        color: Colors.grey,
+        width: 1.0,
+      ),
+      borderRadius: BorderRadius.circular(8.0),
+    ),
+    alignment: Alignment.center,
+    height: 150,
+    child: Row(
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                constraints: BoxConstraints(minWidth: 100),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Search Term',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 10),
         Column(
-          children: [
-            Container(
-              height: 50,
-              width: 200,
-              color: Colors.red,
-              child: const Text('Search Text box'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Submit'),
             ),
           ],
         ),
-        const SizedBox(width: 20),
+        SizedBox(width: 10),
         Column(
-          children: [
-            Container(
-              height: 50,
-              width: 75,
-              color: Colors.yellow,
-              child: const Text('Submit'),
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+              iconSize: 40,
+              icon: Icon(Icons.filter_alt_outlined),
+              onPressed: null,
             ),
           ],
         ),
-        const SizedBox(width: 20),
-        Column(
-          children: [
-            Container(
-              height: 50,
-              width: 50,
-              color: Colors.grey,
-              child: const Text('Filter'),
-            ),
-          ],
-        ),
-      ]));
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
